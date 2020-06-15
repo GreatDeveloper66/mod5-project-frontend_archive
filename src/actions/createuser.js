eb01a53245bbf1c1167c0764c6595b7568cd763e
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-15 17:56:31 -0500

    add createuser and registeringuser actions

---
+++ src/actions/createuser.js
@@ -0,0 +1,8 @@
+export default function createuser() {
+	return function(dispatch) {
+		dispatch({type: 'registeringuser'})
+	}
+	
+	
+	
+}
---
+++ src/reducers/registeringuser.js
@@ -0,0 +1,4 @@
+export default function registeringuser() {
+	dispatch({type: 'registeringuser})
+	
+}
