d2f77677b31827c73806a9e26a69404d86295ec7
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-18 11:35:21 -0500

    Add compose, applyMiddleware, redux Thunk and compose to index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from 'react-redux'
 //import NotesReducer from './reducers/notes'
 import RootReducer from './reducers/rootReducer'
-import { createStore } from 'redux'
-// import thunk from 'redux-thunk'
-// import thunkMiddleware from 'redux-thunk'
+import { createStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk'
 
 // const store = createStore(
 //   RootReducer,
 //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 //   applyMiddleware(thunk)
 // );
-// const store = createStore(RootReducer, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-const store = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(RootReducer, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+//const store = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
