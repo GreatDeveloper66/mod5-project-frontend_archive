7deab7d00f4b2d1d011fa7772aa0c3fad0835d68
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-18 11:52:28 -0500

    Set up fetch Categories Action

--- src/actions/fetchcategories.js
+++ src/actions/fetchcategories.js
@@ -1,6 +1,8 @@
-export default fetchCategories(jwt) {
+import LoadingCategoriesAction from './loadingcategories'
+import LoadCategoriesAction from './loadcategories'
+export default function fetchCategories() {
   dispatch(LoadingCategoriesAction())
-  fetch(`${process.env.REACT_APP_API_URL}/api/v1/categories`,{headers: {Authorization: `Bearer ${jwt}`}})
+  fetch(`${process.env.REACT_APP_API_URL}/api/v1/categories`,{headers: {Authorization: `Bearer ${getState().jwt}`}})
     .then(resp => resp.json())
     .then(data => {
       dispatch(LoadCategoriesAction(data))
