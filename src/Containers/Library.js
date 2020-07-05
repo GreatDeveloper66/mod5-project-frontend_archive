e9fd5a5aeca32180e11a31bcd5af52896b40553e
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-04 23:54:25 -0500

    Add layout and cards to library page

--- src/Containers/Library.js
+++ src/Containers/Library.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, Card } from 'reactstrap'
 
 class Library extends Component {
     render(){
         return(
-            <div>F</div>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            F
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            F
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            F
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+           
         )
     }
 }
