245233001a34f4a4b91fda39b2a8f276326e053f
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-06 13:44:26 -0500

    Add card details to Library component

--- src/Containers/Library.js
+++ src/Containers/Library.js
@@ -8,17 +8,50 @@ class Library extends Component {
                 <Row>
                     <Col>
                         <Card>
-                            F
+                            <CardImg src={require('../images/sanskrit_small.jpg')}/>
+                            <CardBody>
+                                <CardTitle>
+                                    Sanskrit
+                                </CardTitle>
+                                <CardSubtitle>
+                                    Dictionary of sanskrit terms
+                                </CardSubtitle>
+                                <Button>
+                                    View Library
+                                </Button>
+                            </CardBody>
                         </Card>
                     </Col>
                     <Col>
-                        <Card>
-                            F
+                    <Card>
+                            <CardImg src={require('../images/yogi_small.jpg')}/>
+                            <CardBody>
+                                <CardTitle>
+                                    Asanas
+                                </CardTitle>
+                                <CardSubtitle>
+                                    Dictionary of asanas
+                                </CardSubtitle>
+                                <Button>
+                                    View Library
+                                </Button>
+                            </CardBody>
                         </Card>
                     </Col>
                     <Col>
-                        <Card>
-                            F
+                    <Card>
+                            <CardImg src={require('../images/asanas_small.jpg')}/>
+                            <CardBody>
+                                <CardTitle>
+                                    Anatomy
+                                </CardTitle>
+                                <CardSubtitle>
+                                    Dictionary of anatomy terms
+                                </CardSubtitle>
+                                <Button>
+                                    View Library
+                                </Button>
+                            </CardBody>
                         </Card>
                     </Col>
                 </Row>
