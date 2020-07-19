1aba1d23d29117de229dcc3fd05394a28421df2f
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-19 13:39:35 -0500

    Add styling and card to UnderConstruction component

--- src/Containers/UnderConstruction.js
+++ src/Containers/UnderConstruction.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react'
-import { Form, Col, Row, Button, Row, Container, Card, Alert} from 'reactstrap'
+import { Col, Row, Button, Container, Card, CardBody,CardTitle, CardSubtitle } from 'reactstrap'
 
 export default class UnderConstruction extends Component {
     constructor(props){
         super(props)
     }
 
+    bannerStyle = () => ({
+		backgroundImage: `url(${require(`../images/Yogi_Background_C.jpg`)})`,
+		backgroundPosition: 'center',
+		backgroundSize: 'cover',
+		backgroundRepeat: 'no-repeat',
+		height: '100vh',
+		width: '100vw',
+		overflowY: 'hidden'
+	})
+
     render(){
         return(
             <Container className="mt-5">
                 <Row className = "d-flex justify-content-center">
                     <Col xs={12} sm={8} lg={4}>
-                    <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-                        <h1>Yoga Fitness Web App is currently Under Construction for upgrade and maintenance.
+                    <Card inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+                        <CardBody>
+                            <CardTitle>
+                            <h2>The Yoga Fitness Web App is currently Under Construction for upgrade and maintenance.
                             I apologize for any inconvenience.
-                        </h1>
+                            </h2>
+                            </CardTitle>
+                            <CardSubtitle>
+                            <h3>If you would like to see a demo click the button below</h3>
+                            </CardSubtitle>
+                        
+                        
                         <a href="#"><Button>Video Demo</Button></a>
+                       </CardBody>
 
                     </Card>
                     </Col>
