319c46d040addfc0174ba35ff960acf932a0387f
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-26 18:51:17 -0500

    Add redux devtools

--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ import thunkMiddleware from 'redux-thunk'
 // const store = createStore(
 //   RootReducer,
 //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-//   applyMiddleware(thunk)
+ //  applyMiddleware(thunk)
 // );
-//const store = createStore(RootReducer, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(RootReducer, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 //const store = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-const store = createStore(RootReducer, applyMiddleware(thunk))
+//const store = createStore(RootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
