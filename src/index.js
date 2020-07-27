6e314660d51f860b5c36d710aa6a5ac3c6491e78
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-27 17:10:24 -0500

    remove redux dev tools

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ import thunkMiddleware from 'redux-thunk'
 //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
  //  applyMiddleware(thunk)
 // );
-const store = createStore(RootReducer, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+//const store = createStore(RootReducer, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 //const store = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-//const store = createStore(RootReducer, applyMiddleware(thunk))
+const store = createStore(RootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
